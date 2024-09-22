61ae1f2ec582dde6cfb1ec645228a53a8ed3ee4c
Nick <npackullmccormi7020@conestogac.on.ca>
2024-09-22 18:27:51 -0400

    Added Functionality for Reading StudentData.txt and loading data into vector

--- Assignment2NickPM/Source.cpp
+++ Assignment2NickPM/Source.cpp
@@ -1,8 +1,54 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <sstream>
+#include <fstream>
+
+
 using namespace std;
+
+
+struct STUDENT_DATA
+{
+	string firstName;
+	string lastName;
+};
+
+
 
 int main(void)
 {
+	string StudentDataPath = "StudentData.txt";
+	ifstream StudentDataIn;
+	vector<STUDENT_DATA> StudentData;
 
+
+	StudentDataIn.open(StudentDataPath);
+	if (StudentDataIn.is_open())
+	{
+		string line;
+		STUDENT_DATA temp;
+		while (!StudentDataIn.eof())
+		{
+			getline(StudentDataIn, line);
+
+			istringstream sline(line);
+			string fname;
+			getline(sline, fname, ',');
+			temp.firstName = fname;
+			string lname;
+			getline(sline, lname);
+			temp.lastName = lname;
+
+			StudentData.push_back(temp);
+		}
+		
+	}
+	else
+	{
+		cout << "Error Reading File" << endl;
+	}
+	StudentDataIn.close();
 
 	return 1;
 }
